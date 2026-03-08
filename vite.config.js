88ee5f147fcc2a609cf01fa17c5899dd4fda4b0b
aaravgargggg <aaravxgarg@gmail.com>
2026-03-09 00:47:00 +0530

    Vite updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        team: resolve(__dirname, "team.html"),
+        register: resolve(__dirname, "register.html"),
+        aboutschool: resolve(__dirname, "about-school.html"),
+        login: resolve(__dirname, "login.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        contact: resolve(__dirname, "contact.html"),
+        councils: resolve(__dirname, "agendas.html"),
+        admin: resolve(__dirname, "admin.html"),
+        aboutsymposium: resolve(__dirname, "about-symposium.html"),
+        documents: resolve(__dirname, "guides.html")
+      }
+    }
   },
   server: {
     port: 3000,
