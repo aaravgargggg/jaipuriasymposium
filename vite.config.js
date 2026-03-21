d88d1dcae8294b92a7418bfe54d8443348eb17a4
aaravgargggg <aaravxgarg@gmail.com>
2026-03-21 15:27:55 +0530

    Update1

--- vite.config.js
+++ vite.config.js
@@ -8,18 +8,18 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main:           resolve(__dirname, 'index.html'),
-        team:           resolve(__dirname, 'team.html'),
-        register:       resolve(__dirname, 'register.html'),
-        aboutschool:    resolve(__dirname, 'about-school.html'),
-        login:          resolve(__dirname, 'login.html'),
-        dashboard:      resolve(__dirname, 'dashboard.html'),
-        contact:        resolve(__dirname, 'contact.html'),
-        councils:       resolve(__dirname, 'agendas.html'),
-        admin:          resolve(__dirname, 'admin.html'),
-        aboutsymposium: resolve(__dirname, 'about-symposium.html'),
-        sponsors:       resolve(__dirname, 'sponsors.html'),
-        guides:         resolve(__dirname, 'guides.html'),
+        main: resolve(__dirname, "index.html"),
+        team: resolve(__dirname, "team.html"),
+        register: resolve(__dirname, "register.html"),
+        aboutschool: resolve(__dirname, "about-school.html"),
+        login: resolve(__dirname, "login.html"),
+        dashboard: resolve(__dirname, "dashboard.html"),
+        contact: resolve(__dirname, "contact.html"),
+        councils: resolve(__dirname, "agendas.html"),
+        admin: resolve(__dirname, "admin.html"),
+        aboutsymposium: resolve(__dirname, "about-symposium.html"),
+        sponsors: resolve(__dirname, "sponsors.html"),
+        documents: resolve(__dirname, "guides.html")
       }
     }
   },
