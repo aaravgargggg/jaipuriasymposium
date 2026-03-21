3d556ca3ebe28f43c868f7d069817d31ea93cb3a
aaravgargggg <aaravxgarg@gmail.com>
2026-03-21 15:16:55 +0530

    Updated vite file

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   root: '.',
@@ -7,22 +8,28 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        team: resolve(__dirname, "team.html"),
-        register: resolve(__dirname, "register.html"),
-        aboutschool: resolve(__dirname, "about-school.html"),
-        login: resolve(__dirname, "login.html"),
-        dashboard: resolve(__dirname, "dashboard.html"),
-        contact: resolve(__dirname, "contact.html"),
-        councils: resolve(__dirname, "agendas.html"),
-        admin: resolve(__dirname, "admin.html"),
-        aboutsymposium: resolve(__dirname, "about-symposium.html"),
-        sponsors: resolve(__dirname, "sponsors.html"),
-        documents: resolve(__dirname, "src/assets"),
-        documents: resolve(__dirname, "guides.html")
+        main:           resolve(__dirname, 'index.html'),
+        team:           resolve(__dirname, 'team.html'),
+        register:       resolve(__dirname, 'register.html'),
+        aboutschool:    resolve(__dirname, 'about-school.html'),
+        login:          resolve(__dirname, 'login.html'),
+        dashboard:      resolve(__dirname, 'dashboard.html'),
+        contact:        resolve(__dirname, 'contact.html'),
+        councils:       resolve(__dirname, 'agendas.html'),
+        admin:          resolve(__dirname, 'admin.html'),
+        aboutsymposium: resolve(__dirname, 'about-symposium.html'),
+        sponsors:       resolve(__dirname, 'sponsors.html'),
+        guides:         resolve(__dirname, 'guides.html'),
       }
     }
   },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        { src: 'assets/**/*', dest: 'assets' },
+      ]
+    })
+  ],
   server: {
     port: 3000,
     open: true
